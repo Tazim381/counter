547300dfb0160b38df30097128337de63664ffd5
Tazim381 <jucse28.381@gmail.com>
2023-01-18 02:54:05 +0600

    file added

--- script.js
+++ script.js
@@ -8,6 +8,22 @@ function calculateGrade() {
   if (marks >= 80 && marks <= 100) {
     document.getElementById("grade").innerText = "A+";
   }
+  if(marks >=70 && marks <= 79) {
+    document.getElementById("grade").innerText="A";
+  }
+  if (marks >=60 && marks <= 69) {
+    document.getElementById("grade").innerText = "A-";
+  }
+  if(marks >=50 && marks <= 59) {
+    document.getElementById("grade").innerText="B";
+  }
+  if (marks >= 40 && marks <= 49) {
+    document.getElementById("grade").innerText = "C";
+  }
+  if(marks >=0 && marks <= 39) {
+    document.getElementById("grade").innerText="F";
+  }
+
 }
 
 setInitialValue();
