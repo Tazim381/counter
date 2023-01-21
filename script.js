bb6138c693ee832e2f60bb77b76f389d7e3ca71b
Tazim381 <jucse28.381@gmail.com>
2023-01-21 11:11:00 +0600

    duplicate checked

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 
+"use strict";
 
+function input() {
+   const data = document.getElementById("value").value;
+   return data;
+}
+function doesExists(data) {
+
+   const items = [...document.getElementsByTagName('h1')];
+   const item = items.find(function(item) {
+     return (item.innerText === data);
+   });
+   return Boolean(item);
+}
+
+function createElement(data) {
+   const item = document.createElement('h1');
+   item.innerText = data;
+   return item;
+}
 function addElement() {
 
-   const data = document.getElementById("value").value;
-   const element = document.createElement('h1');
-   element.innerText = data;
+   const data = input();
+   if (data ==="") {
+      alert("Emty string ");
+      return;
+   }
+   if(doesExists(data)) {
+      alert("string already exists");
+      return;
+   }
+   const element = createElement(data);
+ 
    const targetElement = document.getElementById("list");
    targetElement.appendChild(element);
 }
